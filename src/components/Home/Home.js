bef058086dbfbfc6b5cbd25a1578f4be654a8b0f
unknown <tasnimtanzim55@gmail.com>
2022-04-29 23:25:13 +0600

    Another Section Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -39,9 +39,50 @@ const Home = () => {
                 </Slider>
             </div>
 
+            {/* Looking For  */}
+            <section className='bg-[#E5EAED]'>
+                <div className='text-center'>
+                    <p className='text-3xl pt-8'>
+                        What are you looking for?
+                    </p>
+                    <p className='text-base mt-4 text-slate-500 car-deals'>Best Car Deals</p>
+                </div>
+                <div className='flex px-20 gap-12 text-center mt-8'>
+                    <div className='p-12 border-r-[1px]'>
+                        <div>
+                            <p className='bg-orange-300 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
+                            '>$</p>
+                            <FontAwesomeIcon icon={faCarSide} className='fa-3x '></FontAwesomeIcon>
+
+                        </div>
+                        <p className='my-4'>Car Sale</p>
+                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
+                    </div>
+                    <div className='p-12 border-r-[1px]'>
+                        <div>
+                            <p className='bg-amber-500 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
+                            '><FontAwesomeIcon icon={faKey}></FontAwesomeIcon></p>
+                            <FontAwesomeIcon icon={faCaravan} className='fa-3x '></FontAwesomeIcon>
+
+                        </div>
+                        <p className='my-4'>Car Buy</p>
+                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
+                    </div>
+                    <div className='p-12'>
+                        <div>
+                            <p className='bg-emerald-500 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
+                            '>R</p>
+                            <FontAwesomeIcon icon={faTaxi} className='fa-3x '></FontAwesomeIcon>
+
+                        </div>
+                        <p className='my-4'>Car Rent</p>
+                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
+                    </div>
+
+                </div>
+            </section>
 
             {/* Our CArs  */}
-
             <section className='mt-12'>
                 <div className='text-center'>
                     <p className='text-3xl'>
@@ -69,47 +110,44 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className='bg-[#E5EAED]'>
-                <div className='text-center'>
-                    <p className='text-3xl pt-8'>
-                        What are you looking for?
-                    </p>
-                    <p className='text-base mt-4 text-slate-500 car-deals'>Best Car Deals</p>
-                </div>
-                <div className='flex px-20 gap-12 text-center mt-8'>
-                    <div className='p-12 border-r-[1px]'>
-                       <div>
-                            <p className='bg-orange-300 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
-                            '>$</p>
-                            <FontAwesomeIcon icon={faCarSide} className='fa-3x '></FontAwesomeIcon>
-                            
-                        </div>
-                        <p className='my-4'>Car Sale</p>
-                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
+            {/* Excellent Dealership  */}
+            <section className='delearship px-20 py-28'>
+                <div className='flex '>
+                    <div className='text-white pr-48 basis-1/2'>
+                        <p className='text-2xl text-orange-400 uppercase tracking-wider'>Vintage Exotic Cars</p>
+                        <p className='text-5xl font-extrabold pt-8'>EXCELLENT
+                            CAR DEALERSHIP</p>
+                        <p className='py-10'>Corify is a leading two-sided digital automotive dealership that connects car shoppers with sellers. Launched in 1998 and headquartered in Chicago, the company empowers shoppers with the data, resources and digital tools needed to make informed buying decisions and seamlessly connect with automotive retailers.</p>
+                        <p className='signature text-4xl'> Pricila</p>
+                        <p className='py-2 text-orange-400'>Tasnim Pricila</p>
+                        <p className='text-sm'>Director of Vintage Exotic
+                        </p>
                     </div>
-                    <div className='p-12 border-r-[1px]'>
-                       <div>
-                            <p className='bg-amber-500 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
-                            '><FontAwesomeIcon icon={faKey}></FontAwesomeIcon></p>
-                            <FontAwesomeIcon icon={faCaravan} className='fa-3x '></FontAwesomeIcon>
-                            
+                    <div className='grid grid-cols-2 text-white gap-20 place-content-center'>
+                        <div>
+                            <p className='text-6xl font-bold mb-4 text-orange-400 relative'>
+                                500+</p>
+                            <p className=''>VEHICLES AVAILABLE RIGHT NOW</p>
                         </div>
-                        <p className='my-4'>Car Buy</p>
-                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
-                    </div>
-                    <div className='p-12'>
-                       <div>
-                            <p className='bg-emerald-500 rounded-full w-[50px] h-[45px] text-center font-[900] p-2 text-white text-2xl relative left-[55%]
-                            '>R</p>
-                            <FontAwesomeIcon icon={faTaxi} className='fa-3x '></FontAwesomeIcon>
-                            
+                        <div>
+                            <p className='text-6xl font-bold mb-4 text-orange-400'>250+</p>
+                            <p>HAPPY &#38; TRUSTED CUSTOMERS</p>
                         </div>
-                        <p className='my-4'>Car Rent</p>
-                        <p className='text-[#74787B]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, unde!</p>
-                    </div>
+                        <div>
+                            <p className='text-6xl font-bold mb-4 text-orange-400'>114</p>
+                            <p>CAR MAKES AND MODELS
 
+                            </p>
+                        </div>
+                        <div>
+                            <p className='text-6xl font-bold mb-4 text-orange-400'>52</p>
+                            <p>RECOGNITION AND AWARDS</p>
+                        </div>
+                    </div>
                 </div>
             </section>
+
+
         </>
     );
 };
