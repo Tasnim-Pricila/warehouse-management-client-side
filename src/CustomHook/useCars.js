1bb0c723d2cab6a4c9287e771506f5d4d5afb4e6
unknown <tasnimtanzim55@gmail.com>
2022-05-01 20:13:04 +0600

    Quantity decrease added

--- src/CustomHook/useCars.js
+++ src/CustomHook/useCars.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 const useCars = (url) => {
     const [cars, setCars] = useState([]);
+
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
--- src/Private/SingleItem/SingleItem.js
+++ src/Private/SingleItem/SingleItem.js
@@ -5,10 +5,72 @@ import useCars from '../../CustomHook/useCars';
 const SingleItem = () => {
 
     const {id} = useParams();
-    const [cars] = useCars(`http://localhost:5000/cars/${id}`);
+    const [cars, setCars] = useCars(`http://localhost:5000/cars/${id}`);
     const { name, description, price, quantity, vendor, img} = cars;
+    
+    
+    const handleDelivered = () => {
+        let updatedQuantity ;
+        if(quantity >= 1){
+            updatedQuantity = quantity - 1;
+        }
+        else if (quantity < 1){
+            updatedQuantity = 0;
+        }
+        
+        console.log(updatedQuantity);
+        const updated = {
+            name: name,
+            description: description,
+            price: price,
+            quantity: updatedQuantity,
+            vendor: vendor,
+            img: img
+        };
 
-    const [decrease, setDecrease] = useState();
+        // Update data
+        fetch(`http://localhost:5000/cars/${id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updated),
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('Succes:', data);
+            setCars(updated);
+           
+        })
+    }
+    const handleIncrease = () => {
+        let updatedQuantity ;
+       
+        updatedQuantity = quantity + 1;
+        console.log(updatedQuantity);
+        const updated = {
+            name: name,
+            description: description,
+            price: price,
+            quantity: updatedQuantity,
+            vendor: vendor,
+            img: img
+        };
+
+        // Update data
+        fetch(`http://localhost:5000/cars/${id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updated),
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('Succes:', data);
+            setCars(updated);
+        })
+    }
     
     return (
         <div className='my-12'>
@@ -22,7 +84,8 @@ const SingleItem = () => {
                     <p className='text-3xl my-6 text-orange-600 font-bold italic'>${price}</p>
                     <p >{description}</p>
                     <p className='my-4 text-orange-400 font-semibold'>Quantity: {quantity}</p>
-                    <button className='mt-4 py-2 border-4 border-amber-400 text-center cursor-pointer w-1/4'> Delivered </button>
+                    <button className='mt-4 py-2 border-4 border-amber-400 text-center cursor-pointer w-1/4' onClick={handleDelivered}> Delivered </button>
+                    <button className='mt-4 py-2 border-4 border-amber-400 text-center cursor-pointer w-1/4' onClick={handleIncrease}> Increase </button>
                 </div>
             </div>
         </div>
