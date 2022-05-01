111369c4dbd71c863310d118239e58a9de847926
unknown <tasnimtanzim55@gmail.com>
2022-05-01 20:33:30 +0600

    Restock Quantity added

--- src/Private/SingleItem/SingleItem.js
+++ src/Private/SingleItem/SingleItem.js
@@ -43,10 +43,11 @@ const SingleItem = () => {
            
         })
     }
-    const handleIncrease = () => {
-        let updatedQuantity ;
-       
-        updatedQuantity = quantity + 1;
+    
+    const handleIncrease = (e) => {
+        e.preventDefault();
+        const quantityValue = parseInt(e.target.quantity.value);
+        const updatedQuantity = quantityValue + parseInt(quantity) ;
         console.log(updatedQuantity);
         const updated = {
             name: name,
@@ -67,16 +68,16 @@ const SingleItem = () => {
         })
         .then(res => res.json())
         .then(data => {
-            console.log('Succes:', data);
             setCars(updated);
+            e.target.reset();
         })
     }
     
     return (
         <div className='my-12'>
-            <div className='grid grid-cols-2 px-20 gap-12'>
+            <div className='grid grid-cols-2 px-20 gap-12 mb-6'>
                 <div>
-                    <img src={img} alt="" className='rounded-lg'/>
+                    <img src={img} alt="" className='rounded-lg w-[600px] h-auto'/>
                 </div>
                 <div className='flex flex-col justify-center'>
                     <p className='text-3xl text-orange-600'>{name}</p>
@@ -85,8 +86,16 @@ const SingleItem = () => {
                     <p >{description}</p>
                     <p className='my-4 text-orange-400 font-semibold'>Quantity: {quantity}</p>
                     <button className='mt-4 py-2 border-4 border-amber-400 text-center cursor-pointer w-1/4' onClick={handleDelivered}> Delivered </button>
-                    <button className='mt-4 py-2 border-4 border-amber-400 text-center cursor-pointer w-1/4' onClick={handleIncrease}> Increase </button>
+                    
                 </div>
+            </div>
+            <hr />
+            <div className='mx-auto w-1/4 mt-12'>
+                <p className='text-center text-2xl my-4 text-blue-600'>Restock Items</p>
+                <form onSubmit={handleIncrease} className='flex flex-col'>
+                    <input type="number" name="quantity" id="" className='border-2 rounded py-1 bg-slate-100 pl-4 border-gray-800' placeholder='How many car you want to restock?' required/>
+                    <input type="submit" value="Increase Quantity" className='border-2 my-4 py-1 cursor-pointer border-orange-400' />
+                </form>
             </div>
         </div>
     );
