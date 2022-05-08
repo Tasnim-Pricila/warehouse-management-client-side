0b9874cd5f18939ea9a1154261c3eaf05058607a
unknown <tasnimtanzim55@gmail.com>
2022-05-08 19:22:11 +0600

    EMail not verified page updated

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -30,12 +30,12 @@ const RequireAuth = ({ children }) => {
     }
 
     if (!user.emailVerified && user.providerData[0].providerId === 'password') {
-        return <div className='text-center h-[90vh] flex flex-col justify-center items-center bg-violet-200'>
-            <p className='text-6xl mb-8 font-semibold text-slate-700 '>OPPS ! </p>
-            <p className='text-red-500 text-6xl font-semibold'>Your Email is not Verified !!!
+        return <div className='text-center min-h-[90vh] flex flex-col justify-center items-center bg-violet-200 py-12 px-8'>
+            <p className='text-6xl mb-8 font-semibold text-slate-700'>OPPS ! </p>
+            <p className='text-red-500 md:text-6xl text-3xl font-semibold'>Your Email is not Verified !!!
             </p>
             <FontAwesomeIcon icon={faSadTear} className='text-slate-500 text-6xl my-8'></FontAwesomeIcon>
-            <button onClick={handleVErification} className='py-4 px-4 my-4 text-white bg-orange-400 hover:bg-orange-700 rounded-3xl text-2xl w-1/3'>Send Verification Mail Again...</button>
+            <button onClick={handleVErification} className='py-4 px-4 my-4 text-white bg-orange-400 hover:bg-orange-700 rounded-3xl text-2xl md:w-1/3'>Send Verification Mail Again...</button>
             <small> <FontAwesomeIcon icon={faRefresh} className='pr-2'></FontAwesomeIcon>
                 Please reload this page after completing your verification.</small>
         </div>
